866ecc4685c619f70012ed53f9fc5c39ef2fa461
Pasha <9139136@gmail.com>
2021-05-25 22:22:34 +0300

    25_05 moresel

--- Lesson_09/Lesson_selenium.py
+++ Lesson_09/Lesson_selenium.py
@@ -23,32 +23,72 @@ driver = webdriver.Chrome(executable_path='./chromedriver')
 # print(f'search by fullXpath: {button_4.text}')
 
 
-driver.get('https://google.com')
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 
-link = driver.find_element_by_css_selector('div#SIvCob a')  # пробел обозначает вложиность
-link.click()
-el = driver.find_element(By.NAME, "q")  # равносильно el = driver.find_element_by_name("q")
+# dr = webdriver.Remote(command_executor='http://127.0.0.1:4444/wd/hub',
+#                       desired_capabilities=webdriver.DesiredCapabilities.FIREFOX)
 
-el.click()
-el.send_keys('Python')
-sleep(1)
-button = driver.find_element_by_class_name('gNO89b')
-button.click()
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 def is_element_present(driver, by, locator):
-    """Проверка"""
     try:
         driver.find_element(by, locator)
     except NoSuchElementException:
         return False
     return True
 
-wait = WebDriverWait
+dr = webdriver.Chrome()
+wait = WebDriverWait(dr, 2)
+actions = webdriver.ActionChains(dr)
+ta = webdriver.TouchActions(dr)
 
-sleep(1)
-links = driver.find_elements_by_partial_link_text('Python')
+# dr.implicitly_wait(5)
+dr.get("https://google.com")
 
-# assert is_element_present(driver, By.c)
+assert is_element_present(dr, By.CLASS_NAME, "gNO89b")
+dr.find_element()
+# link = dr.find_element_by_css_selector("div#SIvCob a")
+# link.click()
 
-driver.close()
+el = dr.find_element_by_css_selector('.gLFyf.gsfi[type="text"]')
+el.clear()
+el.send_keys("python")
+
+el.send_keys(Keys.HOME)
+# el.send_keys(Keys.ARROW_LEFT)
+el.send_keys("sdfsdf")
+
+
+# time.sleep(2)
+
+# button = dr.find_element_by_class_name("gNO89b")
+
+button = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "gNO89b")),
+                    message="Can't find clicable element")
+
+print(button.location)
+print(button.size)
+
+button.click()
+
+time.sleep(2)
+links = dr.find_elements_by_partial_link_text("Python")
 print(len(links))
+
+for link in links:
+    print(link.text)
+
+
+dr.find_elements_by_xpath("//a[contains(text(), 'google')]")
+dr.find_element_by_xpath('//*[@id="gsr"]')
+
+
+
+# dr.close()
+# dr.quit()
